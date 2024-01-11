19b89bc78edcad7188bdaba35628a4e8d6251f01
Adesanya Abdullateef Adebowale <debo@abdullateet.dev>
2024-01-11 22:08:39 +0100

    Add files via upload

---
+++ nested and loop2.js
@@ -0,0 +1,6 @@
+//nested loops - one inside another
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + " times " + secondNum + " equals " + firstNum * secondNum);
+    }
+}
